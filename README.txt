99f7fdaf35bef11ce11b006b57a0e0eb4e47459e
israe <israe@Raeliss.mshome.net>
2024-09-30 21:01:26 +0200

    Commit 4

--- README.txt
+++
@@ -1,2 +0,0 @@
-Hola, este es el proyecto a entregar, tenia otro pero estaba mal hecho.
-Menos mal que me di cuenta.
--- src/main/GitTutorial.java
+++ src/main/GitTutorial.java
@@ -3,9 +3,8 @@ package main;
 public class GitTutorial {
 
 	public static void main(String[] args) {
-		System.out.println("Hola mundo, primer commit");
-		System.out.println("Creamos la branch 1");
-		System.out.println("Cambio 3 en branch 1");
+		System.out.println("Hola mundo");
+		System.out.println("Commit 4");
 	}
 	
 }
